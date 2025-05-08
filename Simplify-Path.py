55f6bc29d908185462d6f9669ca5a515a9aee722
karim abidi <157648055+abidikarim@users.noreply.github.com>
2025-05-08 14:54:42 +0100

    [71] [Time Beats: 15.24%] [Memory Beats: 29.42%] - LeetPush

---
+++ Simplify-Path.py
@@ -0,0 +1,17 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        s=\\
+        for i in range(len(path)):
+            if path[i] == '/' and i > 0 and path[i-1] =='/':
+                continue
+            s+=path[i]
+        ops=s.split('/')
+        stack=[]
+        for op in ops:
+            if op==\..\:
+                if len(stack):
+                    stack.pop()
+            elif op not in ['.','']:
+                stack.append(f\{op}\)
+        return '/' + '/'.join(stack)
+        
