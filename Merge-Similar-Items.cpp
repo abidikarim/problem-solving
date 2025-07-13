3cd514bda9f28d1360cb9df995edffb6e70e3dca
karim abidi <157648055+abidikarim@users.noreply.github.com>
2025-07-13 11:46:37 +0100

    [2363] [Time Beats: 83.99%] [Memory Beats: 18.37%] - LeetPush

--- Merge-Similar-Items.cpp
+++ Merge-Similar-Items.cpp
@@ -2,11 +2,17 @@ class Solution {
 public:
     vector<vector<int>> mergeSimilarItems(vector<vector<int>>& v1, vector<vector<int>>& v2) {
       map<int,int>mp;
-      for(auto item:{v1,v2}) {
-        for(auto &p:item ) {
-            mp[p[0]] += p[1];
-        }
-      }
+    //   for(auto item:{v1,v2}) {
+    //     for(auto &p:item ) {
+    //         mp[p[0]] += p[1];
+    //     }
+    //   }
+    for(auto e:v1){
+        mp[e[0]]+=e[1];
+    }
+    for(auto e:v2){
+        mp[e[0]]+=e[1];
+    }
 
         vector<vector<int>>ans;
         for(auto &i:mp){
