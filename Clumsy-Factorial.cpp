bfd3923128a27f7576d2e58cc52a0ea653aa9d53
karim abidi <157648055+abidikarim@users.noreply.github.com>
2025-07-13 08:50:01 +0100

    [1006] [Time Beats: 43.25%] [Memory Beats: 28.78%] - LeetPush

--- Clumsy-Factorial.cpp
+++ Clumsy-Factorial.cpp
@@ -5,7 +5,7 @@ public:
       int op=0;
       st.push(n);
       for(int i=n-1;i>0;i--) {
-        if(op ==0) st.top() *=i;
+        if(op ==0) st.top()*=i;
         else if(op ==1 ) st.top()/=i;
         else st.push(op == 2 ? i :-i);
         op =(op+1)%4;
