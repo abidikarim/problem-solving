1a7d9b5355baab9a06aff5ef07fbfd55b4f71ed2
karim abidi <157648055+abidikarim@users.noreply.github.com>
2025-09-24 13:52:51 +0100

    [2264] [Time Beats: 100.00%] [Memory Beats: 98.23%] - LeetPush

--- Largest-3-Same-Digit-Number-in-String.py
+++ Largest-3-Same-Digit-Number-in-String.py
@@ -1,8 +1,14 @@
 class Solution:
     def largestGoodInteger(self, num: str) -> str:
-        same_digits_numbers = ["999","888","777","666","555","444","333","222","111","000"]
+        # same_digits_numbers = ["999","888","777","666","555","444","333","222","111","000"]
+        # for s in same_digits_numbers:
+        #     if s in num:
+        #         return s
+        # return ""
+        max_digit = ""
 
-        for s in same_digits_numbers:
-            if s in num:
-                return s
-        return ""
+        for i in range(len(num) -2):
+            if num[i] == num[i+1] and num[i] == num[i+2]:
+                max_digit = max(max_digit, num[i])
+
+        return max_digit * 3
