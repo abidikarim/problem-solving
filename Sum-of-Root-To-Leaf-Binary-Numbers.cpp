1f5d569b793cc392b639e342e6b2e70f70719cc8
karim abidi <157648055+abidikarim@users.noreply.github.com>
2025-09-30 09:55:58 +0100

    [1022] [Time Beats: 5.52%] [Memory Beats: 5.17%] - LeetPush

---
+++ Sum-of-Root-To-Leaf-Binary-Numbers.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<string> getBinaryCodes(TreeNode* root){
+        if(!root) return {};
+        if(!root->left && !root->right){
+            return {to_string(root->val)};
+        }
+        vector<string>codes;
+        for(auto child:{root->left, root->right}){
+            for(string &s:getBinaryCodes(child)){
+                if(child){
+                    codes.push_back(to_string(root->val)+s);
+                }
+            }
+        }
+        return codes;
+    }
+    int sumRootToLeaf(TreeNode* root) {
+        auto codes = getBinaryCodes(root);
+        int ans=0;
+        for(auto s:codes){
+            ans+= stoi(s, nullptr, 2);
+        }
+        return ans;
+    }
+};
