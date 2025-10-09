1986b73ba7002217c8477c065b789bcead8f6c3f
karim abidi <157648055+abidikarim@users.noreply.github.com>
2025-10-09 14:39:05 +0100

    [671] [Time Beats: 100.00%] [Memory Beats: 77.45%] - LeetPush

--- Second-Minimum-Node-In-a-Binary-Tree.cpp
+++ Second-Minimum-Node-In-a-Binary-Tree.cpp
@@ -11,17 +11,20 @@
  */
 class Solution {
 public:
-    set<int>st;
-    void dfs(TreeNode* r){
+    long ans = LONG_MAX;
+    void dfs(TreeNode* r, int minVal){
         if(!r) return;
-        st.insert(r->val);
-        if(r->left) dfs(r->left);
-        if(r->right) dfs(r->right);
+
+        if(r->val > minVal && r->val < ans){
+            ans = (long) r->val;
+            return;
+        }
+
+        if(r->left) dfs(r->left, minVal);
+        if(r->right) dfs(r->right, minVal);
     }
     int findSecondMinimumValue(TreeNode* root) {
-        dfs(root);
-        if(st.size() == 1) return -1;
-        auto it = next(st.begin());
-        return *it;
+        dfs(root, root->val);
+        return ans == LONG_MAX ? -1 : int(ans);
     }
 };
