4085976f4237f5cf7c1842fda6cfdecc6c8f9b59
karim abidi <157648055+abidikarim@users.noreply.github.com>
2025-10-09 10:03:38 +0100

    [589] [Time Beats: 79.07%] [Memory Beats: 19.42%] - LeetPush

---
+++ N-ary-Tree-Preorder-Traversal.cpp
@@ -0,0 +1,35 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int>ans;
+    void dfs(Node* r){
+        if(!r) return;
+        ans.push_back(r->val);
+        for(Node* n:r->children){
+            dfs(n);
+        }
+    }
+    vector<int> preorder(Node* root) {
+        dfs(root);
+        return ans;
+    }
+};
