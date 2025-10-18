01a6cb3c3bb137c6b5f776f87bfaef0b410b3341
karim abidi <157648055+abidikarim@users.noreply.github.com>
2025-10-18 21:46:23 +0100

    [49] [Time Beats: 21.97%] [Memory Beats: 48.41%] - LeetPush

--- Group-Anagrams.cpp
+++ Group-Anagrams.cpp
@@ -2,28 +2,16 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>>ans;
-        map<string, vector<int>>mp;
-        set<int>st;
+        map<string, vector<string>>mp;
         int n = strs.size();
         for(int i=0; i<n; i++){
-            string sortedString = strs[i];       // make a copy
+            string sortedString = strs[i];
             sort(sortedString.begin(), sortedString.end());
-            mp[sortedString].push_back(i);
+            mp[sortedString].push_back(strs[i]);
         }
         for(auto [key, val]:mp){
-            multiset<string>st;
-            for(int i=0; i<val.size(); i++){
-                st.insert(strs[val[i]]);
-            }
-            vector<string> v(st.begin(), st.end());
-            ans.push_back(v);
+            ans.push_back(val);
         }
-
-        sort(ans.begin(), ans.end(), 
-                    [](const vector<string>& a, const vector<string>& b) {
-                        return a.size() < b.size();  // smaller size first
-                    });
-        // sort(v.begin(), v.end());
         return ans;
     }
 };
