78949c3cac695e8b459a9d74066f603f34561d8d
karim abidi <157648055+abidikarim@users.noreply.github.com>
2025-10-18 18:07:46 +0100

    [929] [Time Beats: 75.97%] [Memory Beats: 25.39%] - LeetPush

---
+++ Unique-Email-Addresses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& emails) {
+        int n = emails.size();
+        set<string>v;
+        for(string email:emails){
+            int p = email.find('@');
+            string domain = email.substr(p);
+            string local = email.substr(0,p);
+            if(local.find('+') != string::npos){
+                local = local.substr(0, local.find('+'));
+            }
+            if(local.find('.') != string::npos){
+                local.erase(remove(local.begin(), local.end(),'.'), local.end());
+            }
+        
+            v.insert(local+domain);
+                
+        }
+        return v.size();
+    }
+};
