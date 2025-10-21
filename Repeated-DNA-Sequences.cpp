17afae1144f983a2dd9b7dd27a2ca036067d2df8
karim abidi <157648055+abidikarim@users.noreply.github.com>
2025-10-21 15:59:23 +0100

    [187] [Time Beats: 37.94%] [Memory Beats: 45.16%] - LeetPush

--- Repeated-DNA-Sequences.cpp
+++ Repeated-DNA-Sequences.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
-        if (s.size() < 10) return {};
         unordered_map<string, int>mp;
         string current;
         for(int i=0; i < s.size(); i++){
@@ -20,21 +19,3 @@ public:
         return v;
     }
 };
-
-
-
-/*
- if((s[j] == 'A' || s[j] == 'C' || s[j] == 'G' || s[j] == 'T') && current.size() != 10){
-                current+=s[j];
-            }else if(s[i] != 'A' || s[i] != 'C' || s[i] != 'G'){
-                current.clear();
-                break;
-            }
-            if(current.size() == 10){
-                st.insert(current);
-                current.clear();
-            }
-
-
-
-*/
