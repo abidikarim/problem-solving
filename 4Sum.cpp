a64308fb335a01cfb5dd402ed2e50758ba677ef2
karim abidi <157648055+abidikarim@users.noreply.github.com>
2025-10-29 12:06:24 +0100

    [18] [Time Beats: 52.01%] [Memory Beats: 55.12%] - LeetPush

---
+++ 4Sum.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>>result;
+        set<vector<int>>st;
+        int n = nums.size();
+
+
+        for(int i=0; i<n-1; i++){
+            if(i>0 && nums[i] == nums[i-1])
+                continue;
+            for(int j=i+1; j<n; j++){
+                if(j > i+1 && nums[j] == nums[j-1])
+                    continue;
+                int st = j+1;
+                int end = n-1;
+                while(st < end){
+                    long long cur = (long long) nums[i] + nums[j] + nums[st] + nums[end];
+                    if(cur == target){
+                        result.push_back({nums[i], nums[j], nums[st++], nums[end--]});
+
+                        while(st<end && nums[st] == nums[st-1]){
+                            st++;
+                        }
+                        while(st<end && nums[end] == nums[end+1]){
+                            end--;
+                        }
+                    }else if(cur < target){
+                        st++;
+                    }else {
+                        end--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};
