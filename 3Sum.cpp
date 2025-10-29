052ed7fb74a9cdf00d41b3b71bfccd869468f07a
karim abidi <157648055+abidikarim@users.noreply.github.com>
2025-10-29 12:30:22 +0100

    [15] [Time Beats: 10.56%] [Memory Beats: 9.24%] - LeetPush

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -4,32 +4,27 @@ public:
         sort(nums.begin(), nums.end());
         int n = nums.size();
         vector<vector<int>>result;
+        set<vector<int>>st;
+
         for(int i=0; i<n; i++){
-            if(i>0 && nums[i] == nums[i-1])
-                continue;
             int l= i+1;
             int r = n-1;
-            while(l < r){
-                int curr = nums[i] + nums[l] + nums[r];
-                if(curr == 0){
-                    result.push_back({nums[i], nums[l], nums[r]});
-                    int leftValue = nums[l];
-                    int rightValue = nums[r];
-
-                    while(l<r && nums[l] == leftValue){
-                        l++;
-                    }
-
-                    while(l<r && nums[r] == rightValue){
-                        r--;
-                    }
-
-                }else if(curr > 0){
+            int target = -nums[i];
+            while(l <r){
+                int curr = nums[l] + nums[r];
+                if(curr == target){
+                    st.insert({nums[i], nums[l++], nums[r--]});
+                }else if(curr > target){
                     r--;
                 }else {
                     l++;
                 }
             }
+        }
+        auto it = st.begin();
+        while(it != st.end()){
+            result.push_back(*it);
+            it++;
         }
         return result;
     }
