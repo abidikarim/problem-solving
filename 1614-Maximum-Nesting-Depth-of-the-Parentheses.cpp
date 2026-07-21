597b885bc4ca67f5c052d3fe9b15221828b33313
karim abidi <157648055+abidikarim@users.noreply.github.com>
2026-07-21 12:27:58 +0100

    [1614] [Time Beats: 100.00%] [Memory Beats: 7.36%] - LeetPush

---
+++ 1614-Maximum-Nesting-Depth-of-the-Parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+       stack<char>st;
+       int ans = 0;
+       for(char &c:s){
+            if(c == '('){
+                st.push(c);
+            }else if(c == ')'){
+                ans = max(ans, (int)st.size());
+                st.pop();
+            }
+       }
+       return ans;
+    }
+};
