3039ff2e4312eb8f485ad9fd1e7f489acfd346c5
karim abidi <157648055+abidikarim@users.noreply.github.com>
2026-08-22 13:37:46 +0100

    [144] [Time Beats: 100.00%] [Memory Beats: 74.86%] - LeetPush

---
+++ 144-Binary-Tree-Preorder-Traversal.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def dfs(self, root, v):
+        if root is None:
+            return None
+        v.append(root.val)
+        self.dfs(root.left, v)
+        self.dfs(root.right, v)
+
+    def preorderTraversal(self, root):
+        v = []
+        self.dfs(root, v)
+        return v
+        
